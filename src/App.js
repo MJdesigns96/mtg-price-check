6df3dbd6eb19bafedbc485d38e11498450958b08
Marcus Jeong <marcus.jeong96@gmail.com>
2023-05-24 21:11:34 -0400

    added scryfall api to search for cards in main app page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import CardCard from "./components/card";
 import Header from "./components/header";
 import styles from './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+        fetch(`https://api.scryfall.com/cards/named?fuzzy=${search}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+      }, [search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e)
+    setSearch(e.target[0].value);
+  }
+
+  let temp = '';
+
   return(
     <>
       <div className="header">
         <Header />
       </div>
+      <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type = "text"
+              name = "card"
+              placeholder = "Enter Card Name"
+              onChange = {e => temp = e.target.value}
+            />
+            <button type="submit">Search</button>
+          </form>
+      </div>
       <div className="card-container">
         <CardCard />
-        <CardCard />
-        <CardCard />
-        <CardCard />
       </div>
-    </>
-    
-    
+    </>    
   );
 }
 
